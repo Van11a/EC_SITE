ee0fda8cd756b979c04e3803a234a89c84cd7efa
van11a <mari.vanicham@gmail.com>
2026-03-20 23:03:13 +0900

    - 戻るボタンを押した際や進む、戻るを繰り返しても選択画像がなくならないように修正 - それに伴うバリデーションの修正 - 更新画面で新しく選択したものが戻るボタンを押した時もしくはバリデーションエラー時に変更前の値に戻ってしまっていたため、選択肢したものになるよう修正

--- src/app/Http/Requests/Admin/KeyVisual/NewRequest.php
+++ src/app/Http/Requests/Admin/KeyVisual/NewRequest.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace App\Http\Requests\Admin\KeyVisual;
-use Illuminate\Http\Request;
+
 use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Validation\ValidationException;
 
 class NewRequest extends FormRequest
 {
@@ -21,7 +23,7 @@ class NewRequest extends FormRequest
             'is_new_window'  => '新規ウインドウ',
             'public_start_date' => '掲載開始日',
             'public_end_date' => '掲載終了日',
-            'display_order'=> '表示順',
+            'display_order' => '表示順',
             'is_display' => '表示／非表示',
         ];
     }
@@ -31,11 +33,12 @@ class NewRequest extends FormRequest
      *
      * @return array<string, mixed>
      */
-    public function rules(Request $request)
+    public function  rules()
     {
         return [
-            'title' => 'required|string',
-            'image' => 'required|max:2000|file|image|mimes:jpeg,png,jpg,pdf',
+            'title' => 'required|string|max:255',
+            'image' => 'required_without:before_image|nullable|image|mimes:jpeg,png,jpg,pdf',
+            'before_image' => 'nullable|string',
             'url' => 'nullable|url',
             'is_new_window' => 'nullable|in:0,1',
             'public_start_date' => 'nullable|date',
@@ -43,11 +46,57 @@ class NewRequest extends FormRequest
             'display_order' => [
                 'required',
                 'integer',
-                Rule::unique('key_visuals')->where(function ($request){
-                    return $request->whereNull('deleted_at');
+                Rule::unique('key_visuals')->where(function ($query) {
+                    return $query->whereNull('deleted_at');
                 })
             ],
             'is_display' => 'required|in:0,1',
+        ];
+    }
+
+    /**
+     * バリデーション前に画像のみ先にアップロード
+     */
+    protected function prepareForValidation()
+    {
+        if ($this->hasFile('image')) {
+            $key_visual = app(\App\Services\Admin\KeyVisualService::class)
+                ->uploadImageToTemporaryServer($this->all(), $this);
+
+            if (isset($key_visual['image'])) {
+                $this->merge(['before_image' => $key_visual['image']]);
+            }
+        }
+    }
+
+    /**
+     * バリデーション失敗時の処理
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $inputs = $this->all();
+        unset($inputs['image']);
+
+        $response = redirect()
+            ->to($this->getRedirectUrl())
+            ->withInput($inputs) // before_imageを保持
+            ->withErrors($validator, $this->errorBag); // エラーメッセージを保持
+
+        // 例外オブジェクトを作成し、作成したレスポンスをセットして投げる
+        $exception = new ValidationException($validator);
+        $exception->response = $response;
+
+        throw $exception;
+    }
+
+    /**
+     * 画像のバリデーションメッセージ
+     */
+    public function messages()
+    {
+        return [
+            'image.required' => ':attributeは必須です。',
+            'image.required_without' => ':attributeは必須です。',
         ];
     }
 }
